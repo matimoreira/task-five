bad90e10497bb485d53f40df31a7feeecb7c43e4
Matias Moreira <moreiramatias10@hotmail.com>
2020-03-07 04:38:00 -0300

    Established a basic style and structure of the page. import the bootstrap cdn's

--- src/App.js
+++ src/App.js
@@ -2,25 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		brands: [
+			{
+				id: 1,
+				marca: 'Fiat',
+				description: 'Auto piola'
+			},
+			{
+				id: 2,
+				marca: 'Ford',
+				description: 'Auto un poco mas piola'
+			}
+		]
+	}
+	render() {
+		return (
+			<div className="container mt-5">
+				<div className="row">
+					<div className="col-md-6">
+						<h3 className="display-5">Marcas</h3>
+						<hr/>
+							<table className="table table-striped bg-white">
+								<thead>
+									<th>Marca</th>
+									<th>Descripcion</th>
+									<th>Accion</th>
+								</thead>
+								<tbody>
+									
+								</tbody>
+							</table>
+						 {/*table: listando todas las marcas*/}
+					</div>
+					<div className="col-md-6">
+						<h3 className="display-5">Añadir Marcas</h3>
+						<hr/>
+					{/*Form: con todos los input necesarios para dar de alta una nueva marca*/}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
