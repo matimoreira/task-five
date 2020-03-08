ec3c44d155ebb2c99a5b468d95b589911c17ff38
Matias Moreira <moreiramatias10@gmail.com>
2020-03-08 20:55:36 -0300

    Agregue funcionabilidades y resolvi como conectarme con la api

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Header from './header/Header';
-import logo from './logo.svg';
 
 class App extends React.Component {
 	title = 'Marcas';
 
 
-	state = {
-		brands: [
-			{
-				id: 1,
-				marca: 'Fiat',
-				description: 'Auto piola'
-			},
-			{
-				id: 2,
-				marca: 'Ford',
-				description: 'Auto un poco mas piola'
-			},
-			{
-				id: 3,
-				marca: 'Toyota',
-				description: '4x4=16'
-			}
-		]
+	state = {brands: [{id: 1,marca: 'Fiat',estado: 1}]};
+
+	handleDeleteBrand = async (index, id) => {
+		let response = await fetch(`https://localhost:5001/api/Vehiclebrand/${id}`, {method: 'DELETE'})
+		console.log(response);
+		console.log(`Id del elemento a eliminar: ${id}`);
+		const newsBrands = this.state.brands;
+		newsBrands.splice(index, 1);
+		this.setState({brands: newsBrands})
 	}
+
+	async componentDidMount() {
+		let response = await fetch("https://localhost:5001/api/Vehiclebrand");
+		let json = await response.json();
+		this.setState( ( json === '' ? { brands: ['Error'] } : { brands: json } ) );
+	}
+
 	render() {
 		return (
 			<div className="container mt-5">
-				<Header title={this.title} rows={this.state.brands}></Header>
+				<Header title={this.title} rows={this.state.brands} deleteBrand={this.handleDeleteBrand}></Header>
 			</div>
 		);
 	}
